10882e40bc610a9895b1a4d019eec0d7f05bc6fd
Mahmood-Darwish <mahmood2000334@gmail.com>
2022-09-26 14:06:44 +0300

    init

--- src/index.js
+++ src/index.js
@@ -1,17 +1,26 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDom from 'react-dom';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+import App from './app';
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+export default () => <App/>;
+
+export const mount = (Component) => {
+    ReactDom.render(
+        <Component/>,
+        document.getElementById('app')
+    );
+
+    if(module.hot) {
+        module.hot.accept('./app', ()=> {
+            ReactDom.render(
+                <App/>,
+                document.getElementById('app')
+            );
+        })
+    }
+};
+
+export const unmount = () => {
+    ReactDom.unmountComponentAtNode(document.getElementById('app'));
+};
